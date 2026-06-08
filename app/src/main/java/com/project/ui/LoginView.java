36e2807cf4a51af7a35891c2e224a0a6c5b61b08
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-06-08 11:24:29 -0300

    loggin hecho - implementar entidad Usuario

--- app/src/main/java/com/project/ui/LoginView.java
+++ app/src/main/java/com/project/ui/LoginView.java
@@ -8,25 +8,63 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.avatar.AvatarVariant;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 
-@Route(value = "login", autoLayout = false) 
+
+@Route(value = "login", autoLayout = false)
 @PageTitle("Login")
-@AnonymousAllowed 
+@AnonymousAllowed
 public class LoginView extends Main implements BeforeEnterObserver {
 
     private final LoginForm login;
 
     public LoginView() {
-        login = new LoginForm();
-        login.setAction("login"); 
 
-        VerticalLayout layout = new VerticalLayout();
+        login = new LoginForm();
+        login.setAction("login");
+
+        H2 titulo = new H2("App Terceros");
+        titulo.getStyle()
+                .set("margin-top", "0")
+                .set("text-align", "center");
+
+        Span subtitulo = new Span("Inicio Sesión");
+        subtitulo.getStyle()
+                .set("color", "var(--lumo-secondary-text-color)");
+        
+        Avatar avatar = new Avatar("AT");
+        avatar.addThemeVariants(
+            AvatarVariant.AURA_FILLED,
+            AvatarVariant.LARGE
+        );
+
+        VerticalLayout card = new VerticalLayout(
+        		avatar,
+                titulo,
+                subtitulo,
+                login
+        );
+
+        card.setAlignItems(FlexComponent.Alignment.CENTER);
+        card.setPadding(true);
+        card.setSpacing(true);
+
+        card.getStyle()
+        .set("border", "1px solid gray")
+        .set("border-radius", "12px")
+        .set("padding", "20px");
+
+        card.setWidth("450px");
+        VerticalLayout layout = new VerticalLayout(card);
+
+        layout.setSizeFull();
         layout.setAlignItems(FlexComponent.Alignment.CENTER);
         layout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
-        layout.add(login);
-        layout.setSizeFull();
 
         add(layout);
         setSizeFull();
@@ -35,10 +73,11 @@ public class LoginView extends Main implements BeforeEnterObserver {
     @Override
     public void beforeEnter(BeforeEnterEvent event) {
         if (event.getLocation()
-                 .getQueryParameters()
-                 .getParameters()
-                 .containsKey("error")) {
-            login.setError(true); 
+                .getQueryParameters()
+                .getParameters()
+                .containsKey("error")) {
+
+            login.setError(true);
         }
     }
 }
