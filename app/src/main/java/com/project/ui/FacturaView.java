141518e093a91dd59be7adcd0aaefc2c93a37ea0
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-06-11 13:13:07 -0300

    prueba definitiva

--- app/src/main/java/com/project/ui/FacturaView.java
+++ app/src/main/java/com/project/ui/FacturaView.java
@@ -26,6 +26,7 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
 
+
 @RolesAllowed({"USER","ADMIN"})
 @Route(value = "facturas", layout = MainLayout.class)
 @PageTitle("Facturas")
